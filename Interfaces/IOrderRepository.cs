f23af1187cb5ce8e094c041aae7cfb4e50c68117
abood.jarrar <abd.a.jarrar@gmail.com>
2026-09-22 15:01:53 +0300

    create the IOrderRepository interface

---
+++ Interfaces/IOrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Asal.OrderReportingSystem.Models;
+
+namespace Asal.OrderReportingSystem.Interfaces
+{
+    public interface IOrderRepository
+    {
+        public bool AddOrder(Guid CustomerId, decimal Amount);
+        public void DisplayAllOrders();
+
+        public Order? GetOrderById(Guid OrderId);
+
+        public List<Order> GetCompletedOrders();
+
+        public List<Order> GetOrdersAbove(decimal Amount);
+        public List<Order> DisplayOrdersWithin(DateTime StartDate, DateTime EndDate);
+
+        public List<Order> GetOrdersBySpecificCustomer(Guid CustomerId);
+
+        public decimal GetOrdersTotalAmount();
+
+        public decimal GetCustomerOrdersTotalAmount();
+
+        public Customer GetCustomerWithHighestOrdersAmount();
+
+        public List<Order> GetOrdersSortedByAmount();
+
+        public List<Order> GetOrdersSortedByDate();
+
+        public void PrintOrder();
+
+        public void PrintOrders(List<Order>orders);
+
+    }
+}
