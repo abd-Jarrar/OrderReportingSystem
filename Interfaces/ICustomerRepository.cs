94ba7f876d8761711331a0092f4f3cb8b5e6a80d
abood.jarrar <abd.a.jarrar@gmail.com>
2026-09-22 15:36:06 +0300

    create teh ICusomterRepository interface

---
+++ Interfaces/ICustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿using Asal.OrderReportingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Asal.OrderReportingSystem.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        public Customer? GetCustomerById(Guid CustomerId);
+
+        public bool DeleteCustomer(Guid CustomerId);
+
+        public List<Order> GetCustomerOrders(Guid CustomerId);
+
+        public bool AddCustomer(string CustomerName, string CustomerEmail);
+
+        public void DisplayCustomer(Guid CustomerId);
+
+        public void DisplayCustomers(List<Customer> customers);
+
+        public void DisplayAllCustomers();
+
+    }
+}
